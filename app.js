58deea18c44c687b67a40a35803f8d75eae260d3
Roger Bloom <rogerbloom1@gmail.com>
2021-01-29 21:28:58 -0800

    exercise 1 done

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+let sum = 0;
+
+function arraySum(numbers) {
+    numbers.forEach((number, index) => sum += number);
+    return sum;
+}
+console.log(arraySum(numbers));
+
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
